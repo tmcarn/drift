e1b96ccef8766e25bc40612f03d6f8575a0f3302
tmcarn <mcarn.t@northeastern.edu>
2026-03-31 16:33:15 -0400

    moveit removed

--- ros2_ws/src/my_package/launch/arm.launch.py
+++ ros2_ws/src/my_package/launch/arm.launch.py
@@ -101,64 +101,6 @@ def generate_launch_description():
     pkg = get_package_share_directory('my_package')
     robot_description = Command(['xacro ', os.path.join(pkg, 'urdf', 'arm.urdf.xacro')])
 
-    srdf_path = os.path.join(pkg, 'config', 'moveit', 'arm.srdf')
-    with open(srdf_path, 'r') as f:
-        robot_description_semantic = f.read()
-
-    kinematics = {
-        'robot_description_kinematics': {
-            'arm': {
-                'kinematics_solver': 'kdl_kinematics_plugin/KDLKinematicsPlugin',
-                'kinematics_solver_search_resolution': 0.005,
-                'kinematics_solver_timeout': 0.05,
-            }
-        }
-    }
-
-    planning = {
-        'planning_pipelines.pipeline_names': ['ompl'],
-        'planning_pipelines.ompl.planning_plugins': ['ompl_interface/OMPLPlanner'],
-        'planning_pipelines.ompl.request_adapters': [
-            'default_planning_request_adapters/ResolveConstraintFrames',
-            'default_planning_request_adapters/ValidateWorkspaceBounds',
-            'default_planning_request_adapters/CheckStartStateBounds',
-            'default_planning_request_adapters/CheckStartStateCollision',
-        ],
-        'planning_pipelines.ompl.response_adapters': [
-            'default_planning_response_adapters/AddTimeOptimalParameterization',
-            'default_planning_response_adapters/ValidateSolution',
-            'default_planning_response_adapters/DisplayMotionPath',
-        ],
-    }
-
-    moveit_controllers = {
-        'moveit_simple_controller_manager': {
-            'controller_names': ['joint_trajectory_controller', 'gripper_controller'],
-            'joint_trajectory_controller': {
-                'type': 'FollowJointTrajectory',
-                'action_ns': 'follow_joint_trajectory',
-                'default': True,
-                'joints': ['shoulder_joint', 'elbow_joint', 'wrist_joint'],
-            },
-            'gripper_controller': {
-                'type': 'GripperCommand',
-                'action_ns': 'gripper_cmd',
-                'default': True,
-                'joints': ['left_finger_joint'],
-            },
-        },
-        'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager',
-    }
-
-    moveit_params = {
-        'robot_description': robot_description,
-        'robot_description_semantic': robot_description_semantic,
-        **kinematics,
-        **planning,
-        **moveit_controllers,
-        'use_sim_time': False,
-    }
-
     state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -190,13 +132,6 @@ def generate_launch_description():
         arguments=['gripper_controller'],
     )
 
-    move_group = Node(
-        package='moveit_ros_move_group',
-        executable='move_group',
-        output='screen',
-        parameters=[moveit_params],
-    )
-
     rviz = Node(
         package="rviz2",
         executable="rviz2",
@@ -206,7 +141,7 @@ def generate_launch_description():
 
     delayed = TimerAction(
         period=3.0,
-        actions=[spawn_jsb, spawn_jtc, spawn_gripper, move_group],
+        actions=[spawn_jsb, spawn_jtc, spawn_gripper],
     )
 
     return LaunchDescription([
